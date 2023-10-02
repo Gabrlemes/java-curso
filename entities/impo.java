dcdcddb9672dc0ec5b119312cb0f48621338623c
Gabrlemes <gabrielsiber816@gmail.com>
2023-10-02 00:50:46 -0300

    void's

--- entities/impo.java
+++ entities/impo.java
@@ -1,2 +1,21 @@
-package entities;public class impo {
+package entities;
+
+public class impo {
+
+    public String name;
+    public double salary;
+    public double impost;
+
+    public double netSalary() {
+        return salary - impost;
+    }
+
+    public void increaseSalary(double percertage) {
+        salary += salary * percertage / 100.0;
+    }
+
+    public String toString() {
+        return name + ", $ " + String.format("%.2f%", netSalary());
+    }
+
 }
--- func/funcionario.java
+++ func/funcionario.java
@@ -1,2 +1,38 @@
-package func;public class funcionario {
+package func;
+
+import java.util.Scanner;
+import java.util.Locale;
+import entities.impo;
+
+public class funcionario {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        impo emp = new impo();
+
+        System.out.print("nome: ");
+        emp.name = sc.nextLine();
+
+        System.out.print("salário bruto: ");
+        emp.salary = sc.nextDouble();
+
+        System.out.print("imposto: ");
+        emp.impost = sc.nextDouble();
+
+        System.out.println();
+        System.out.println("funcionário: " + emp);
+        System.out.println();
+        System.out.print("quantos % será o aumento salárial? ");
+        double percentage = sc.nextDouble();
+        emp.increaseSalary(percentage);
+
+        System.out.println();
+        System.out.print("atualização dos dados: " + emp);
+
+        sc.close();
+
+    }
 }
