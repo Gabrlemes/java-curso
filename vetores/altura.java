c9dbbbdddd245299c05707cd0e5c9a642726c2e9
gabriel <gabrielsiber816@gmail.com>
2023-11-28 17:28:32 -0300

    a

--- vetores/altura.java
+++ vetores/altura.java
@@ -1,2 +1,51 @@
-package vetores;public class altura {
+package vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class altura {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas serão especificadas? ");
+        int n = sc.nextInt();
+
+        String[] nomes = new String[n];
+        int[] idades = new int[n];
+        double[] alturas = new double[n];
+
+        for (int i=0; i<n; i++) {
+            System.out.println("dados da " + (i + 1) + "a pessoa: ");
+            System.out.print("nome: ");
+            nomes[i] = sc.next();
+            System.out.print("idade: ");
+            idades[i] = sc.nextInt();
+            System.out.print("altura: ");
+            alturas[i] = sc.nextDouble();
+        }
+
+        double soma = 0.0;
+        for (int i=0; i<n; i++) {
+            soma += alturas[i];
+        }
+        double mediaAlturas = soma / n;
+
+        System.out.println();
+        System.out.printf("altura média: %.2f%n", mediaAlturas);
+
+        int cont = 0;
+        for (int i=0; i<n; i++) {
+            if (idades[i] < 16) {
+                cont = cont + 1;
+            }
+        }
+
+        double percent = cont * 100.0 / n;
+        System.out.printf("pessoas com menos de 16 anos: %.1f%%%n", percent);
+
+        sc.close();
+    }
 }
